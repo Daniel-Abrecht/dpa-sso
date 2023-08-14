e102ba522bba41010d6c5375382c1fdb8f1b0e67
Daniel Abrecht <public@danielabrecht.ch>
2023-08-14 20:59:38 +0000

    libapache2-mod-auth-dpa-sso package

---
+++ libapache2-mod-auth-dpa-sso/debian-bookworm/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/libapache2-mod-auth-dpa-sso/issues
+# Bug-Submit: https://github.com/<user>/libapache2-mod-auth-dpa-sso/issues/new
+# Changelog: https://github.com/<user>/libapache2-mod-auth-dpa-sso/blob/master/CHANGES
+# Documentation: https://github.com/<user>/libapache2-mod-auth-dpa-sso/wiki
+# Repository-Browse: https://github.com/<user>/libapache2-mod-auth-dpa-sso
+# Repository: https://github.com/<user>/libapache2-mod-auth-dpa-sso.git
